8df827f08f9d9b02d70221c932bb4193ecc2bc1f
shriyansh1495 <shriyansh1495@gmail.com>
2023-04-22 17:45:14 +0530

    Added profile and adhar card design in add user

--- src/app/pages/user/add-user/add-user.component.ts
+++ src/app/pages/user/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RestApiService } from 'src/app/service/rest-api.service';
 
 @Component({
@@ -7,35 +8,69 @@ import { RestApiService } from 'src/app/service/rest-api.service';
   styleUrls: ['./add-user.component.scss']
 })
 export class AddUserComponent implements OnInit {
+  
+  name: String = '';
+  salary: String = '';
+  joining_date: String = '';
+  mobile_number: String = '';
+  email: String = '';
+  address: String = '';
+  allowed_holiday: String = '';
+  notes:String = '';
   imageData;
-  fileBlob: any;
+  imageDataAdharCard;
   userImg:any;
   userAadharImg:any;
-  constructor(public restApi:RestApiService) { }
+  constructor(public restApi:RestApiService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   addUser(){
     let params = {
-'user_name' : 'Darshana',
-  'user_email' : 'darshana@gmail.com',
-    'user_mobile' : '9090989898',
-      'user_address' : 'Thane',
-  'user_salary' : '25000',
-      'user_joiningdate' : '2022-01-10',
-      'user_holiday' : '6',
-      'user_description' : 'Temp user',
+'user_name' : this.name,
+  'user_email' : this.email,
+    'user_mobile' : this.mobile_number,
+      'user_address' : this.address,
+  'user_salary' : this.salary,
+      'user_joiningdate' : this.joining_date,
+      'user_holiday' : this.allowed_holiday,
+      'user_description' : this.notes,
   'user_photo' :this.userImg,
     'user_aadhar' : 'temp',
     }
-    this.restApi.addUser(params).subscribe((data) => {
-      console.log('fetchuserlist -- ',data)
-     if(data["Status"] == "00"){
-      
-     }
-    });
+    setTimeout(() => {
+      console.log('params -- ',params)
+      this.restApi.addUser(params).subscribe((data) => {
+        console.log('fetchuserlist -- ',data)
+       if(data["Status"] == "00"){
+        this.router.navigate(['/user']);
+
+       }
+      });
+   
+    }, 1000);
+   
+    
   }
+
+  onFileSelected(event) {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = e => this.imageData = reader.result;
+       reader.readAsDataURL(file);
+  }
+   }
+
+   onFileSelectedAdharCard(event) {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = e => this.imageDataAdharCard = reader.result;
+       reader.readAsDataURL(file);
+  }
+   }
 
 uploadFile(event, type) {
     if (event.target.value) {
